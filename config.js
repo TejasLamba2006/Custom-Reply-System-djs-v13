76c938523f1d39c4b04ad06c189dc90b1b30a21b
Tejas Lamba <73745640+TajuModding@users.noreply.github.com>
2021-10-10 20:10:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   prefix: "!", //prefix
   Slash: {
-        global: false, //boolean | has to be true or false
-        test: true, //boolean has to be true or false
-        GUILD_ID: "860874073835569154" //string has to be number
+        global: true, //boolean | has to be true or false
+        test: false, //boolean has to be true or false
+        GUILD_ID: "123456789013467" //string has to be number
   },
-  token: "ODU5NzI0MDQ2MDA1OTYwNzA1.YNw2aQ.HtvE6QW9ugROMtgucZweLLRNpWk", // bot token
+  token: "", // bot token
 };
